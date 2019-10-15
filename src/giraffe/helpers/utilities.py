b562f1b77d648916a9d05b99d68fa1b8f637870a
Boris Milner <boris.milner@gmail.com>
2019-10-15 09:50:47 +0300

    Unit testing

---
+++ src/giraffe/helpers/utilities.py
@@ -0,0 +1,34 @@
+import time
+from typing import List
+
+
+def list_as_chunks(the_list: List, chunk_size: int):
+    length_of_iterable = len(the_list)
+    return (the_list[start_slice:start_slice + chunk_size] for start_slice in range(0,
+                                                                                    length_of_iterable,
+                                                                                    chunk_size))
+
+
+class Timer:
+    def __init__(self):
+        self.__start_time = 0
+        self.time_elapsed = 0
+
+    def __enter__(self):
+        self.__reset()
+        self.start()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.time_elapsed = time.time() - self.__start_time
+
+    def __reset(self):
+        self.__start_time = 0
+        self.time_elapsed = 0
+
+    def start(self) -> None:
+        self.__start_time = time.time()
+
+    def stop(self) -> float:
+        second_elapsed = time.time() - self.__start_time
+        self.__reset()
+        return second_elapsed
---
+++ tests/multi_processing/test_neo_multiprocessing.py
@@ -0,0 +1,32 @@
+import time
+from typing import List
+from giraffe.helpers.utilities import Timer
+from giraffe.helpers.neo_multiprocessing import MultiProcHelper
+
+sleep_time_simulation_sec = 0.25
+
+
+def processing_function(batch: List, time_to_sleep: float = sleep_time_simulation_sec):
+    time.sleep(time_to_sleep)  # Simulate some work
+    return len(batch)
+
+
+def test_neo_multi_proc_helper():
+    timer = Timer()
+    test_number_of_items = 1_000_050
+    test_batch_size = 50_000
+    list_to_process = [i for i in range(0, test_number_of_items)]
+
+    number_of_whole_batches = test_number_of_items // test_batch_size
+    last_batch_size = test_number_of_items % test_batch_size
+    expected_number_of_batches = number_of_whole_batches + (1 if last_batch_size != 0 else 0)
+    expected_time_without_multi_processing = expected_number_of_batches * sleep_time_simulation_sec
+
+    mph = MultiProcHelper()
+    with timer:
+        results = mph.multi_process_list_in_chunks(the_list=list_to_process, batch_function=processing_function, batch_size=test_batch_size)
+
+    assert len(results) == expected_number_of_batches
+    assert all(item == results[0] == test_batch_size for item in results[0:-1])
+    assert results[-1] == last_batch_size
+    assert timer.time_elapsed < expected_time_without_multi_processing
