baccee5710c0ef1529670c7403b59286f40f3ed5
Boris Milner <boris.milner@gmail.com>
2019-10-15 14:05:00 +0300

    Configurations

--- src/giraffe/tools/redis_db.py
+++ src/giraffe/tools/redis_db.py
@@ -10,17 +10,13 @@ class RedisDB(object):
         self.log = log_helper.get_logger(logger_name=self.__class__.__name__)
         self.log.debug(f'Initialising redis driver.')
         self._driver = redis.Redis(host=config.redis_host_address, port=config.redis_port)
-        atexit.register(self.__exit__, None, None, None)
+        atexit.register(self._driver.close)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        try:
-            _ = self._driver.client_list()
-            self._driver.close()
-        except redis.ConnectionError:
-            pass
+        self._driver.close()
 
     def get_driver(self):
         return self._driver
