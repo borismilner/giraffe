53b97c351c7885eeb0b01995aca3e5825c695418
Boris Milner <boris.milner@gmail.com>
2019-10-17 08:57:01 +0300

    Refactoring

--- cleanup.py
+++ cleanup.py
@@ -7,11 +7,12 @@ folders_to_delete = [
     '.pytest_cache',
     'giraffe.egg-info'
 ]
+
 if __name__ == '__main__':
 
     for root, dirs, files in os.walk(".", topdown=True):
         for folder_name in dirs:
             folder_path = os.path.join(root, folder_name)
             if folder_name in folders_to_delete:
-                print(folder_path)
+                print(f'Cleaning {folder_path}')
                 shutil.rmtree(folder_path)
